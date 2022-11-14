2b2fa0cfb704ca7cc52c1fe536c978a7e14d3bd6
Shlomit Bernstein <shlomitbern@gmail.com>
2022-11-14 15:29:18 +0200

    Your turn :)

---
+++ Targil0/Program8034.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8034();
+            Welcome3107();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome3107();
+        private static void Welcome8034()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
