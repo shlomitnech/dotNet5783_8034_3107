9b57213c7f433161e385ca4751b9c315c1b668ee
navam <navam@DESKTOP-UMEOD27>
2022-12-05 15:29:06 +0200

    Maybe getting somewhere?

--- DalList/DalOrder.cs
+++ DalList/DalOrder.cs
@@ -1,6 +1,27 @@
-﻿namespace DO;
-namespace Dal;
+﻿namespace Dal;
+using DO;
+
+
 
 public class DalOrder
 {
+    DataSource ds = DataSource.s_Initialize();
+    public void Insert()
+    {
+        if (DataSource.orders.Length == 100)
+        {
+            throw new Exception("Can't take more orders");
+        }
+
+        Order order = new Order();
+        order.ID = DataSource.NextOrderNumber();
+
+        DataSource.orders[DataSource.orders.Length + 1] = order;
+
+    }
+
+    public void Delete()
+    {
+        if(orders.Length == 0)
+    }
 }
