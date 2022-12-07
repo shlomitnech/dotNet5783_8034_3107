698dec11706cf8809163522e09b60b046b37a1bd
navam <navam@DESKTOP-UMEOD27>
2022-12-07 16:35:21 +0200

    Updated checking conditions for order item

--- DalList/DalOrderItems.cs
+++ DalList/DalOrderItems.cs
@@ -7,17 +7,30 @@ public class DalOrderItems
 {
     public int InsertOrderItem(OrderItem current )
     {
-        //Check if the order item already exists
+        bool ifOrderExists = false;
+        bool ifProductExists = false;
+
+        //Check if the order item already exists, and if the order id exists and product id exists
         for (int i = 0; i < (DataSource.countOrderItems); i++)
         {
             if (current.ID == DataSource.orderItems[i].ID)
                 throw new Exception("Item already exists\n");
+
+            if(current.orderID == DataSource.orderItems[i].orderID)
+                ifOrderExists = true;
+
+            if(current.productID == DataSource.orderItems[i].productID)
+                ifProductExists = true;
         }
+
+        //If the order or product does not exist it will throw an exception
+        if (!ifOrderExists) throw new Exception("order does not exist");
+        if (!ifProductExists) throw new Exception("product does not exist");
+        
         //If we already have 200 products then throw error
         if ((DataSource.countOrderItems >= 200)) //DOES this update the order count?
-        {
             throw new Exception("Too many order items!\n");
-        }
+
         //If the orderItem is not out of stock and there is space, insert it into products
         {
             int newID = DataSource.Config.NextOrderItemNumber;
