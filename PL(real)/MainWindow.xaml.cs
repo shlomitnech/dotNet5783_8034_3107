f784e8f88b7a2132a1b9cf1de8a1a64e7aa8e66a
Shlomit Bernstein <shlomitbern@gmail.com>
2023-01-09 15:25:39 +0200

    kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

---
+++ PL(real)/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using BlApi;
+using BlImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BlImplementation;
+
+namespace PL_real_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static IBl bl = new Bl();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowProductsButton_Click(object sender, RoutedEventArgs e) => new ProductListWindow().Show();
+
+    }
+}
