b83b866fcf701f2a90f66b881c70ae9e7a2b2e8f
Shlomit Bernstein <shlomitbern@gmail.com>
2023-01-09 15:47:17 +0200

    Someone who knows vS needs to help me

---
+++ PLNOTWORK/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using BlApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BO;
+using BlImplementation;
+using PL;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static IBl bl = new Bl();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e) => new ViewTheList().Show();
+ 
+    }
+}  
